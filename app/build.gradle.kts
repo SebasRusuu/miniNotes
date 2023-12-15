5436349212557c433b085373c00454afcda80470
Sebastião Rusu <20220905@iade.pt>
2023-12-15 22:48:01 +0000

    Página edit list finalizada

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "pt.iade.sebastiaorusu.atividade"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "pt.iade.sebastiaorusu.atividade"
