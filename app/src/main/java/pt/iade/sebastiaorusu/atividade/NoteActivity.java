4dbbefaebc70cb8c402ee0e3717cfa8e01f62fc8
Sebastião Rusu <20220905@iade.pt>
2023-12-16 19:52:02 +0000

    Add do menu na toolbar

--- app/src/main/java/pt/iade/sebastiaorusu/atividade/NoteActivity.java
+++ app/src/main/java/pt/iade/sebastiaorusu/atividade/NoteActivity.java
@@ -3,34 +3,31 @@ package pt.iade.sebastiaorusu.atividade;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class NoteActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_item_list);
 
         setupComponents();
     }
 
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_item_list, menu);
         return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId() == R.id.add_note){
-            //Action "add" button
-            Intent intent = new Intent(MainActivity.this, NoteActivity.class);
-            startActivity(intent);
-
+        if(item.getItemId() == R.id.save_note){
+            //Action "save"
+            finish();
             return true;
         }
         return super.onOptionsItemSelected(item);
